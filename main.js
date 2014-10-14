593e03957bbf109b97d49955b5e459ff3cd5e5b7
hinamiyagk <hinamiy@npca-1.info>
2014-10-14 18:49:15 +0900

    セルフ神経衰弱の実装

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 var carddata = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10];
 var clickcount = 0;
 var cardimg = [];
-var images;
-var cardback;
+var images,cardback,clearimg;
+var fstclick,secclick;
+var clickedcheck = 0;
 
 window.addEventListener('load', init, false);
 
@@ -18,6 +19,8 @@ function init() {
 	}
 	cardback = document.createElement('img');
 	cardback.src = 'img/cardback.png';
+	clearimg = document.createElement('img');
+	clearimg.src = 'img/clear.png';
 }
 
 function gamestartclick(e) {
@@ -29,6 +32,7 @@ function gamestartclick(e) {
 function restartclick(e) {
 	for (var i = 0; i < images.length; i++) {
 		images[i].src = cardback.src;
+		images[i].classList.add('cardimg');
 	}
 	gamestart();
 }
@@ -48,8 +52,36 @@ function gamestart() {
 }
 
 function imgclick(e) {
-	if (clickcount === 0) {
-		var clickimg = e.target;
-		clickimg.src = cardimg[clickimg.cardid - 1].src;
+	if (clickedcheck === 0) {
+		if (clickcount === 0) {
+			fstclick = e.target;
+			fstclick.src = cardimg[fstclick.cardid - 1].src;
+			fstclick.classList.remove('cardimg');
+			clickcount++;
+		} else {
+			secclick = e.target;
+			if (fstclick !== secclick) {
+				secclick.src = cardimg[secclick.cardid - 1].src;
+				secclick.classList.remove('cardimg');
+				clickcount--;
+				clickedcheck = 1;
+				setTimeout(cardcheck, 1000);
+			}
+		}
 	}
+}
+
+function cardcheck() {
+	if (fstclick.cardid === secclick.cardid) {
+		fstclick.src = clearimg.src;
+		fstclick.removeEventListener('click', imgclick, false);
+		secclick.src = clearimg.src;
+		secclick.removeEventListener('click', imgclick, false);
+	} else {
+		fstclick.src = cardback.src;
+		fstclick.classList.add('cardimg');
+		secclick.src = cardback.src;
+		secclick.classList.add('cardimg');
+	}
+	clickedcheck = 0;
 }
