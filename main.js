c87ff20989d310b39e317030f00f31ea6cdd689d
hinamiyagk <hinamiy@npca-1.info>
2015-01-01 12:00:56 +0900

    DOM操作を改良

--- main.js
+++ main.js
@@ -80,48 +80,48 @@ var game = {
 	start: function() {
 		var cardData = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12];
 		var length = cardData.length;
-		var i;
+		var i,a,j;
 		var cards = document.getElementById('cards');
 		//shuffle by Fisher-Yates algorithm
 		for (i = length - 1; i > 0; i--) {
-			var j = Math.floor(Math.random() * (i + 1));
-			var a = cardData[i];
+			j = Math.floor(Math.random() * (i + 1));
+			a = cardData[i];
 			cardData[i] = cardData[j];
 			cardData[j] = a;
 		}
 		computer.memory = [];
 
-		var images = document.querySelectorAll('#cards img');
-		length = images.length;
-		for (i = 0; i < length; i++) {
-			cards.removeChild(images[i]);
+		function removePlayerCards() {
+			var plcards = document.getElementById('plcards');
+			var node = plcards.cloneNode(false);
+			plcards.parentNode.replaceChild(node, plcards);
 		}
+		removePlayerCards();
 
-		var plcards = document.querySelectorAll('#plcards img');
-		var plcardholder = document.getElementById('plcards');
-		length = plcards.length;
-		for (i = 0; i < length; i++) {
-			plcardholder.removeChild(plcards[i]);
+		function removeComputerCards() {
+			var comcards = document.getElementById('comcards');
+			var node = comcards.cloneNode(false);
+			comcards.parentNode.replaceChild(node, comcards);
 		}
+		removeComputerCards();
 
-		var comcards = document.querySelectorAll('#comcards img');
-		var comcardholder = document.getElementById('comcards');
-		length = comcards.length;
-		for (i = 0; i < length; i++) {
-			comcardholder.removeChild(comcards[i]);
-		}
-
-		var img;
-		for (i = 0; i < 24; i++) {
-			img = document.createElement('img');
-			img.src = pictures.back.src;
-			img.id = 'card' + i;
-			img.style.left = (i % 8) * 110 + 'px';
-			img.style.top = Math.floor(i / 8) * 150 + 'px';
-			img.addEventListener('click', imgclick, false);
-			img.cardid = cardData[i];
-			cards.appendChild(img);
-		}
+		(function () {
+			var node = cards.cloneNode(false);
+			var img, style;
+			var imgtemplate = document.createElement('img');
+			imgtemplate.src = pictures.back.src;
+			for (var i = 0; i < 24; i++) {
+				img = imgtemplate.cloneNode(false);
+				img.id = 'card' + i;
+				img.addEventListener('click', imgclick, false);
+				style = img.style;
+				style.left = (i % 8) * 110 + 'px';
+				style.top = Math.floor(i / 8) * 150 + 'px';
+				img.cardid = cardData[i];
+				node.appendChild(img);
+			}
+			document.getElementById('game').replaceChild(node, cards);
+		}) ();
 
 		player.score = 0;
 		computer.score = 0;
@@ -154,15 +154,10 @@ var game = {
 	}
 };
 
-window.addEventListener('load', init, false);
-
-function init() {
-	var button = document.getElementById('startbtn');
-	button.addEventListener('click', gamestartClick, false);
-	button = document.getElementById('restartbtn');
-	button.addEventListener('click', restartClick, false);
-	button = document.getElementById('backtitle');
-	button.addEventListener('click', backtoTitle, false);
+window.addEventListener('load', function() {
+	document.getElementById('startbtn').addEventListener('click', gamestartClick, false);
+	document.getElementById('restartbtn').addEventListener('click', restartClick, false);
+	document.getElementById('backtitle').addEventListener('click', backtoTitle, false);
 	pictures.front = [];
 	for (var i = 0; i < 12; i++) {
 		pictures.front[i] = document.createElement('img');
@@ -170,7 +165,7 @@ function init() {
 	}
 	pictures.back = document.createElement('img');
 	pictures.back.src = 'img/cardback.png';
-}
+}, false);
 
 function gamestartClick(e) {
 	//check input
@@ -225,65 +220,64 @@ function restartClick(e) {
 }
 
 function backtoTitle() {
+	var undef;//undef is undefined
 	document.getElementById('title').style.display = 'block';
 	document.getElementById('game').style.display = 'none';
-	player.input.order = undefined;
-	player.input.level = undefined;
+	player.input.order = undef;
+	player.input.level = undef;
 }
 
 function imgclick(e) {
-	if (player.flag.canclick) {
-		if (player.flag.clickcount === 0) {
-			//first time
-			player.click.first = e.target;
-			player.click.first.src = pictures.front[player.click.first.cardid - 1].src;
-			player.flag.clickcount++;
-		} else {
-			//second time
-			player.click.second = e.target;
-			if (player.click.first !== player.click.second) {
-				player.click.second.src = pictures.front[player.click.second.cardid - 1].src;
-				player.flag.clickcount--;
-				player.flag.canclick = false;
-				setTimeout(function() {
-					if (player.click.first.cardid === player.click.second.cardid) {
-						//if cards are same number
-						var cards = document.getElementById('cards');
-						cards.removeChild(player.click.first);
-						cards.removeChild(player.click.second);
-						//show image in score
-						var img1;
-						img1 = document.createElement('img');
-						img1.src = pictures.front[player.click.first.cardid - 1].src;
-						img1.className = "countcards";
-						img1.style.left = player.score * 20 + 'px';
+	if (!player.flag.canclick) return;
+	if (player.flag.clickcount === 0) {
+		//first time
+		player.click.first = e.target;
+		player.click.first.src = pictures.front[player.click.first.cardid - 1].src;
+		player.flag.clickcount = 1;
+	} else {
+		//second time
+		player.click.second = e.target;
+		if (player.click.first === player.click.second) return;
+		player.click.second.src = pictures.front[player.click.second.cardid - 1].src;
+		player.flag.clickcount = 0;
+		player.flag.canclick = false;
+		setTimeout(function() {
+			if (player.click.first.cardid === player.click.second.cardid) {
+				//if cards are same number
+				var cards = document.getElementById('cards');
+				cards.removeChild(player.click.first);
+				cards.removeChild(player.click.second);
+				//show image in score
+				var img1;
+				img1 = document.createElement('img');
+				img1.src = pictures.front[player.click.first.cardid - 1].src;
+				img1.className = 'countcards';
+				img1.style.left = player.score * 20 + 'px';
 
-						var img2;
-						img2 = document.createElement('img');
-						img2.src = pictures.front[player.click.second.cardid - 1].src;
-						img2.className = "countcards";
-						img2.style.left = (player.score + 1) * 20 + 'px';
+				var img2;
+				img2 = document.createElement('img');
+				img2.src = pictures.front[player.click.second.cardid - 1].src;
+				img2.className = 'countcards';
+				img2.style.left = (player.score + 1) * 20 + 'px';
 
-						var plimgs = document.getElementById('plcards');
-						plimgs.appendChild(img1);
-						plimgs.appendChild(img2);
+				var plimgs = document.getElementById('plcards');
+				plimgs.appendChild(img1);
+				plimgs.appendChild(img2);
 
-						player.score = player.score + 2;
-						document.getElementById('plcount').innerHTML = player.score;
-						player.flag.canclick = true;
+				player.score = player.score + 2;
+				document.getElementById('plcount').innerHTML = player.score;
+				player.flag.canclick = true;
 
-						if (player.score + computer.score === 24) {
-							setTimeout(game.end, 500);
-						}
-					} else {
-						//if cards are different number
-						player.click.first.src = pictures.back.src;
-						player.click.second.src = pictures.back.src;
-						document.getElementById('turn').innerHTML = '相手の番です';
-						setTimeout(computer.play, 500);
-					}
-				}, 500);
+				if (player.score + computer.score === 24) {
+					setTimeout(game.end, 500);
+				}
+			} else {
+				//if cards are different number
+				player.click.first.src = pictures.back.src;
+				player.click.second.src = pictures.back.src;
+				document.getElementById('turn').innerHTML = '相手の番です';
+				setTimeout(computer.play, 500);
 			}
-		}
+		}, 500);
 	}
 }
